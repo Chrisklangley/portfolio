e3029a880300a492417f3a76c4f4b470956fbf6a
Christopher langley <christopherlangley@Christophers-MacBook-Pro.local>
2023-06-23 12:13:37 -0700

    new project added

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Projects.css";
+import { AiFillGithub } from "react-icons/ai";
+import { FaDev } from "react-icons/fa";
 
 function Project() {
   return (
@@ -8,14 +10,57 @@ function Project() {
         <h1>Projects</h1>
         <div className="project-container">
           <div className="proj">
-            <h2>GrooveBurner</h2>
+            <h2> Project #2: GrooveBurner </h2>
             <p>
               A react application that allows a user to view and create a
               playlist using the spotify API
             </p>
+            <div>
+              <a
+                className="git-link"
+                target="_blank"
+                href="https://github.com/Chrisklangley/Specs-Capstone"
+              >
+                {" "}
+                github <AiFillGithub />
+              </a>
+            </div>
           </div>
           <iframe
             src="https://www.youtube.com/embed/rm0arzR2DpM?start=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div className="project-container sweep">
+          <div className="proj">
+            <h2> Project #1: Expense Tracker </h2>
+            <p>A react application that tracks a users Expenses</p>
+            <div>
+              <div className="project-links">
+                <a
+                  className="git-link"
+                  target="_blank"
+                  href="https://github.com/Chrisklangley/expenses-react"
+                >
+                  {" "}
+                  github <AiFillGithub />
+                </a>
+                <a
+                  target="_blank"
+                  href="https://expenses2023.herokuapp.com/"
+                  className="demo-link"
+                >
+                  {" "}
+                  live demo <FaDev />
+                </a>
+              </div>
+            </div>
+          </div>
+          <iframe
+            src="https://www.youtube.com/embed/rm0arzR2Dp=1"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
